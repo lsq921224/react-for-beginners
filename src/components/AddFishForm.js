b2c0658c83c8a1b940a86351abae862b0c429679
Peter Shengqian Liu <shengqian.liu@appfolio.com>
2016-12-08 00:07:42 -0800

    Add addfishform and fish state

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  createFish(event) {
+    event.preventDefault();
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
+    };
+    this.props.addFish(fish);
+    this.fishForm.reset();
+  }
+
 	render() {
 		return (
-			<h2>AddFishForm</h2>
+			<form ref={ (form) => this.fishForm = form } className="fish-edit" onSubmit={ (e) => this.createFish(e) }>
+        <input type="text" ref={ (input) => this.name = input } placeholder="Fish Name" />
+        <input type="text" ref={ (input) => this.price = input } placeholder="Fish Price" />
+        <select ref={ (input) => this.status = input }>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <input type="text" ref={ (input) => this.desc = input } placeholder="Fish Desc" />
+        <input type="text" ref={ (input) => this.image = input } placeholder="Fish Image" />
+        <button type="submit">+ Add Item</button>
+      </form>
 		)
 	}
 }
