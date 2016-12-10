e406747cf21430b595665c0d84e3dc2dff35acde
Peter Shengqian Liu <shengqian.liu@appfolio.com>
2016-12-09 20:43:58 -0800

    Add Fish class and display a list of fishes on app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
@@ -17,7 +18,7 @@ class App extends React.Component {
   addFish = (fish) => {
     const fishes = { ...this.state.fishes };
     const timeStamp = Date.now();
-    fishes[`fish-${timeStamp}`] = fish;
+    fishes[ `fish-${timeStamp}` ] = fish;
     this.setState({ fishes });
   };
 
@@ -32,6 +33,13 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fish">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={ key } details={ this.state.fishes[key] } />)
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addFish={ this.addFish } loadSamples={ this.loadSamples } />
