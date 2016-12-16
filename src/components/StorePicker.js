bbdd8587070660879e608c901fba49bedce3a870
Peter Shengqian Liu <shengqian.liu@appfolio.com>
2016-12-16 14:52:07 -0800

    Fix store picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore = event =>  {
+    event.preventDefault();
+    console.log('You Changed the URL');
+    // first grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`)
+    // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ e => this.goToStore(e) }>
         { /* here */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={ input => { this.storeInput = input} } />
         <button type="submit">Visit Store -> </button>
       </form>
     )
